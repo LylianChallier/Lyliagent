d553185131fab7ce89b21a918ca61afa80dc56db
Lylian Challier <lylianchallier@MacBook-Air-de-Lylian.local>
2025-10-31 00:54:40 +0100

    init commit

--- app/agents/app.py
+++
@@ -1,6 +0,0 @@
-from langchain.chat_models import init_chat_model
-from app.config import MISTRAL_API_KEY
-
-
-model = init_chat_model("mistral-small-latest", model_provider="mistralai", api_key=MISTRAL_API_KEY)
-print(model.invoke([{"role": "user", "content": "Hello world"}]).content)
